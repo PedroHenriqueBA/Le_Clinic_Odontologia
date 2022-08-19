2041092628a10ad5fe16596b2ba78fed37c22821
PedroHenriqueBA <pedrohenriquebelchioralves@gmail.com>
2022-08-19 12:39:11 -0300

    Le Clinic Odonto

---
+++ script.js
@@ -0,0 +1,353 @@
+window.onload = () => {
+    var swiper = new Swiper(".carousel", {
+        slidesPerView: 1,
+        spaceBetween: 200,
+        slidesPerGroup: 1,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+    });
+
+    var swiper = new Swiper(".content2", {
+        slidesPerView: 1,
+        spaceBetween: 0,
+        slidesPerGroup: 1,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+
+        breakpoints:{
+          400:{
+            slidesPerView: 4
+          }
+        }
+    });
+
+  }
+
+
+
+  var modal_0 = document.getElementById('modal');
+  var link_0 = document.getElementById('barra');
+  var close_0 = document.getElementById('closeBtn');
+  
+  link_0.addEventListener('click', openModal_0);
+  
+  function openModal_0(){
+    modal_0.style.display = 'block';
+  }
+  close_0.addEventListener('click', closeModal_0);
+  
+  function closeModal_0(){
+    modal_0.style.display = 'none';
+  }
+
+
+
+  var modal_01 = document.getElementById('modal01');
+  var link_01 = document.getElementById('barra1');
+  var close_01 = document.getElementById('closeBtn01');
+  
+  link_01.addEventListener('click', openModal_01);
+  
+  function openModal_01(){
+    modal_01.style.display = 'block';
+  }
+  close_01.addEventListener('click', closeModal_01);
+  
+  function closeModal_01(){
+    modal_01.style.display = 'none';
+  }
+
+
+
+  var modal_02 = document.getElementById('modal02');
+  var link_02 = document.getElementById('barra2');
+  var close_02 = document.getElementById('closeBtn02');
+  
+  link_02.addEventListener('click', openModal_02);
+  
+  function openModal_02(){
+    modal_02.style.display = 'block';
+  }
+  close_02.addEventListener('click', closeModal_02);
+  
+  function closeModal_02(){
+    modal_02.style.display = 'none';
+  }
+
+
+
+  var modal_03 = document.getElementById('modal03');
+  var link_03 = document.getElementById('barra3');
+  var close_03 = document.getElementById('closeBtn03');
+  
+  link_03.addEventListener('click', openModal_03);
+  
+  function openModal_03(){
+    modal_03.style.display = 'block';
+  }
+  close_03.addEventListener('click', closeModal_03);
+  
+  function closeModal_03(){
+    modal_03.style.display = 'none';
+  }
+
+
+
+  var modal_04 = document.getElementById('modal04');
+  var link_04 = document.getElementById('barra4');
+  var close_04 = document.getElementById('closeBtn04');
+  
+  link_04.addEventListener('click', openModal_04);
+  
+  function openModal_04(){
+    modal_04.style.display = 'block';
+  }
+  close_04.addEventListener('click', closeModal_04);
+  
+  function closeModal_04(){
+    modal_04.style.display = 'none';
+  }
+
+
+
+  var modal_05 = document.getElementById('modal05');
+  var link_05 = document.getElementById('barra5');
+  var close_05 = document.getElementById('closeBtn05');
+  
+  link_05.addEventListener('click', openModal_05);
+  
+  function openModal_05(){
+    modal_05.style.display = 'block';
+  }
+  close_05.addEventListener('click', closeModal_05);
+  
+  function closeModal_05(){
+    modal_05.style.display = 'none';
+  }
+
+
+
+  var modal_06 = document.getElementById('modal06');
+  var link_06 = document.getElementById('barra6');
+  var close_06 = document.getElementById('closeBtn06');
+  
+  link_06.addEventListener('click', openModal_06);
+  
+  function openModal_06(){
+    modal_06.style.display = 'block';
+  }
+  close_06.addEventListener('click', closeModal_06);
+  
+  function closeModal_06(){
+    modal_06.style.display = 'none';
+  }
+
+
+
+  var modal_07 = document.getElementById('modal07');
+  var link_07 = document.getElementById('barra7');
+  var close_07 = document.getElementById('closeBtn07');
+  
+  link_07.addEventListener('click', openModal_07);
+  
+  function openModal_07(){
+    modal_07.style.display = 'block';
+  }
+  close_07.addEventListener('click', closeModal_07);
+  
+  function closeModal_07(){
+    modal_07.style.display = 'none';
+  }
+
+
+
+  var modal_08 = document.getElementById('modal08');
+  var link_08 = document.getElementById('barra8');
+  var close_08 = document.getElementById('closeBtn08');
+  
+  link_08.addEventListener('click', openModal_08);
+  
+  function openModal_08(){
+    modal_08.style.display = 'block';
+  }
+  close_08.addEventListener('click', closeModal_08);
+  
+  function closeModal_08(){
+    modal_08.style.display = 'none';
+  }
+
+
+var modal = document.getElementById('modal9');
+var link = document.getElementById('barra9');
+var close = document.getElementById('closeBtn9');
+
+link.addEventListener('click', openModal);
+
+function openModal(){
+  modal.style.display = 'block';
+}
+close.addEventListener('click', closeModal);
+
+function closeModal(){
+  modal.style.display = 'none';
+}
+
+
+var modal_10 = document.getElementById('modal10');
+var link_10 = document.getElementById('barra10');
+var close_10 = document.getElementById('closeBtn10');
+
+link_10.addEventListener('click', openModal_10);
+
+function openModal_10(){
+  modal_10.style.display = 'block';
+}
+close_10.addEventListener('click', closeModal_10);
+
+function closeModal_10(){
+  modal_10.style.display = 'none';
+}
+
+
+var modal_101 = document.getElementById('modal101');
+var link_101 = document.getElementById('barra101');
+var close_101 = document.getElementById('closeBtn101');
+
+link_101.addEventListener('click', openModal_101);
+
+function openModal_101(){
+  modal_101.style.display = 'block';
+}
+close_101.addEventListener('click', closeModal_101);
+
+function closeModal_101(){
+  modal_101.style.display = 'none';
+}
+
+
+var modal_11 = document.getElementById('modal11');
+var link_11 = document.getElementById('barra11');
+var close_11 = document.getElementById('closeBtn11');
+
+link_11.addEventListener('click', openModal_11);
+
+function openModal_11(){
+  modal_11.style.display = 'block';
+}
+close_11.addEventListener('click', closeModal_11);
+
+function closeModal_11(){
+  modal_11.style.display = 'none';
+}
+
+
+var modal_12 = document.getElementById('modal12');
+var link_12 = document.getElementById('barra12');
+var close_12 = document.getElementById('closeBtn12');
+
+link_12.addEventListener('click', openModal_12);
+
+function openModal_12(){
+  modal_12.style.display = 'block';
+}
+close_12.addEventListener('click', closeModal_12);
+
+function closeModal_12(){
+  modal_12.style.display = 'none';
+}
+
+
+var modal_13 = document.getElementById('modal13');
+var link_13 = document.getElementById('barra13');
+var close_13 = document.getElementById('closeBtn13');
+
+link_13.addEventListener('click', openModal_13);
+
+function openModal_13(){
+  modal_13.style.display = 'block';
+}
+close_13.addEventListener('click', closeModal_13);
+
+function closeModal_13(){
+  modal_13.style.display = 'none';
+}
+
+
+var modal_14 = document.getElementById('modal14');
+var link_14 = document.getElementById('barra14');
+var close_14 = document.getElementById('closeBtn14');
+
+link_14.addEventListener('click', openModal_14);
+
+function openModal_14(){
+  modal_14.style.display = 'block';
+}
+close_14.addEventListener('click', closeModal_14);
+
+function closeModal_14(){
+  modal_14.style.display = 'none';
+}
+
+
+var modal_15 = document.getElementById('modal15');
+var link_15 = document.getElementById('barra15');
+var close_15 = document.getElementById('closeBtn15');
+
+link_15.addEventListener('click', openModal_15);
+
+function openModal_15(){
+  modal_15.style.display = 'block';
+}
+close_15.addEventListener('click', closeModal_15);
+
+function closeModal_15(){
+  modal_15.style.display = 'none';
+}
+
+
+var modal_16 = document.getElementById('modal16');
+var link_16 = document.getElementById('barra16');
+var close_16 = document.getElementById('closeBtn16');
+
+link_16.addEventListener('click', openModal_16);
+
+function openModal_16(){
+  modal_16.style.display = 'block';
+}
+close_16.addEventListener('click', closeModal_16);
+
+function closeModal_16(){
+  modal_16.style.display = 'none';
+}
+
+
+var modal_17 = document.getElementById('modal17');
+var link_17 = document.getElementById('barra17');
+var close_17 = document.getElementById('closeBtn17');
+
+link_17.addEventListener('click', openModal_17);
+
+function openModal_17(){
+  modal_17.style.display = 'block';
+}
+close_17.addEventListener('click', closeModal_17);
+
+function closeModal_17(){
+  modal_17.style.display = 'none';
+}
